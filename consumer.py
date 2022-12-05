04d70ad8e16d120a100f76ccc60b4ddce9b6ee17
ariel marte <30759534+arielmarte@users.noreply.github.com>
2022-12-04 23:50:36 -0300

    Entrega da Atividade - Consumidor

--- consumer.py
+++ consumer.py
@@ -2,13 +2,18 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
+print('Simulando topicos de notificação de um equipamento que monitora a cada segundo as condições de uma sala de servidores e a disponibilidade da rede elétrica\n')
+print('Qual informação deseja consumir?\n')
+print('1 - Temperatura\n')
+print('2 - Umidade\n')
+print('3 - Tensão\n')
+opcao = int(input('Opção: '))
+
+topico = ''
+topico = 'temperatura' if (opcao == 1) else ('umidade' if(opcao == 2) else ( 'tensao' if(opcao == 3) else ''))
+
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-try:
-  topic = sys.argv[1]
-except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
-  
-consumer.subscribe([topic])
+
+consumer.subscribe([topico])
 for msg in consumer:
-    print (msg.value)
+    print(msg.value.decode())
